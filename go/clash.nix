d84be676955a8c9cd3b9b77f8a3170c09e1315c4
Mark Pashmfouroush <mark@markpash.me>
2023-03-18 15:12:06 +0000

    clash: 1.13.0 -> 1.14.0
    Diff: https://github.com/Dreamacro/clash/compare/v1.13.0...v1.14.0

--- go/clash.nix
+++ go/clash.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "clash";
-  version = "1.13.0";
+  version = "1.14.0";
 
   src = fetchFromGitHub {
     owner = "Dreamacro";
     repo = "clash";
     rev = "v${version}";
-    sha256 = "sha256-f/iSnSaRr1dqMRKb7GDZdc2WuykO42XMSNKwMOwuagc=";
+    sha256 = "sha256-w/Iz1PZekeKVGFHPteMEbjLP3V9qMmLLAz27qW0VtPk=";
   };
 
   CGO_ENABLED = 0;
 
-  vendorSha256 = "sha256-fDn6UlijI2TJPF4FS50u1MMDxnd8eDTbqHLnGso/FoU=";
+  vendorSha256 = "sha256-raNFt+Ymh7m+p1wXy1ofMO1UJ2EouwaY7Ysngfw3X8U=";
 
   ldflags = [
     "-s"
