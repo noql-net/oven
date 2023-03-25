48f7d1019735372cbc257c79fbb2321cd54aaf57
Mark Pashmfouroush <mark@markpash.me>
2023-03-25 00:54:18 +0000

    brook: 20230401 -> 20230404
    Diff: https://github.com/txthinking/brook/compare/v20230401...v20230404

--- go/brook.nix
+++ go/brook.nix
@@ -2,18 +2,18 @@
 
 buildGoModule rec {
   pname = "brook";
-  version = "20230401";
+  version = "20230404";
 
   src = fetchFromGitHub {
     owner = "txthinking";
     repo = "brook";
     rev = "v${version}";
-    sha256 = "sha256-PVkdK1wcY3PaeixEG4StV1AmYPit6pQNErcALmV2LXc=";
+    sha256 = "sha256-nh59sfOWLk4evuIxa35bWu0J6xSUIzrPv4oQHWSZInA=";
   };
 
   CGO_ENABLED = 0;
 
-  vendorSha256 = "sha256-7h+rMfFFpcsfZa6tw/o0uRIDw4g3g+dwd9y2Ysg2NJc=";
+  vendorSha256 = "sha256-PYdR0vfgOgRheHDvoIC9jCFmfzCSOhxqcPFm+MqirsQ=";
 
   ldflags = [ "-s" "-w" "-buildid=" ];
   subPackages = [ "cli/brook" ];
