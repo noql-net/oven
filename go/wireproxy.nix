79f2c6b54cf3fd851fb22dfb879e2d02b1736aa3
Mark Pashmfouroush <mark@markpash.me>
2023-05-22 23:09:00 +0000

    wireproxy: 1.0.5 -> 1.0.6
    Diff: https://github.com/octeep/wireproxy/compare/v1.0.5...v1.0.6

--- go/wireproxy.nix
+++ go/wireproxy.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "wireproxy";
-  version = "1.0.5";
+  version = "1.0.6";
 
   src = fetchFromGitHub {
     owner = "octeep";
     repo = "wireproxy";
     rev = "v${version}";
-    hash = "sha256-5xyKmFxXYhrR8EbG1/ByD10lhkPT9Ky1lq+LL2djaao=";
+    hash = "sha256-Sy8jApnU3dpsXi5vWyEY6D250xpG73aByNZ/pSg90l0=";
   };
 
   CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-/LZs6N2m5nHx735Ug+PcM1I1ZL9f8VYEpd7Tt4WizMQ=";
+  vendorHash = "sha256-LBLEb2oVi5ILNtoOtmJZ7NC7hMvLZcexYAxwmb4iUBo=";
 
   ldflags = [
     "-s"
