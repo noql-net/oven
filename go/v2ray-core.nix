8fc6e49f45947c6a1f18abc40a22c33be32e0522
Mark Pashmfouroush <mark@markpash.me>
2023-05-24 00:56:30 +0000

    v2ray-core: 5.4.1 -> 5.5.0
    Diff: https://github.com/v2fly/v2ray-core/compare/v5.4.1...v5.5.0

--- go/v2ray-core.nix
+++ go/v2ray-core.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "v2ray-core";
-  version = "5.4.1";
+  version = "5.5.0";
 
   src = fetchFromGitHub {
     owner = "v2fly";
     repo = "v2ray-core";
     rev = "v${version}";
-    hash = "sha256-LrsLkuLoiZb3Y5BUkg9lB+qd0HHbgRNrixm5BdQMKck=";
+    hash = "sha256-Lh8FbB6QRkuD/mxjVeJ7SzGj7LSONdmSvs+uqNhYODw=";
   };
 
   CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-0uc+oncdOdIhIygr2ktT7f1qHIeyoVEnjyhzsUMjMBY=";
+  vendorHash = "sha256-M8wf1ZihngrJt+Asg9SoRT0M5CtFf/ihY5HqtoUXbNI=";
 
   ldflags = [
     "-s"
