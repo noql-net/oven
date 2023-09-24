c759b419ab561a922199dc47b6b8e7a2b6b259cc
Mark Pashmfouroush <mark@markpash.me>
2023-09-24 21:23:09 +0000

    xray-knife: 1.5.9 -> 1.5.10
    Diff: https://github.com/lilendian0x00/xray-knife/compare/v1.5.9...v1.5.10

--- go/xray-knife.nix
+++ go/xray-knife.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "xray-knife";
-  version = "1.5.9";
+  version = "1.5.10";
 
   src = fetchFromGitHub {
     owner = "lilendian0x00";
     repo = "xray-knife";
     rev = "v${version}";
-    hash = "sha256-55YBluaaUvaRowcommJhhvt1KbOx+ZtsmMkTrNWpDJw=";
+    hash = "sha256-zPnZMDV2jwIXF7aei8Vw8RFdqMqHH+nz5D5l0Mwz7cU=";
   };
 
   CGO_ENABLED = 0;
