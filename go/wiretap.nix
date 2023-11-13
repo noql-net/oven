d63e6b5fdd174b3b3850fed0e0bc16a55929c770
Mark Pashmfouroush <mark@markpash.me>
2023-11-13 08:22:45 +0000

    wiretap: 0.4.1 -> 0.4.2
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- go/wiretap.nix
+++ go/wiretap.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "wiretap";
-  version = "0.4.1";
+  version = "0.4.2";
 
   src = "${fetchFromGitHub {
     owner = "sandialabs";
     repo = "wiretap";
     rev = "v${version}";
-    hash = "sha256-u9+R+JCbHXF/PzqNelATvS1Sg0+cnRHmIHkye5bswVI=";
+    hash = "sha256-b9Yvua5vOrbej547mNCP5QgtaLf69T47GR2Ok5i22Y0=";
   }}/src";
 
   CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-MgGBw/d9xCRwafOn8+GHdROOvyvg1LdZgKh+Y83Bg5k=";
+  vendorHash = "sha256-/rITemxVMIPpNVbqnIJ+0NswlBU65qrVZEWWfk/sC8s=";
 
   ldflags = [
     "-s"
