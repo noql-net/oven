d33116892c7701b2c8902912439423efc8de041a
Mark Pashmfouroush <mark@markpash.me>
2023-11-28 05:55:31 +0000

    fix tuic build
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- rust/tuic.nix
+++ rust/tuic.nix
@@ -11,6 +11,7 @@ buildRustPackage rec {
     hash = "sha256-VoNr91vDqBlt9asT/dwCeYk13UNiDexNNiKwD5DSn8k=";
   };
 
+  cargoExtraArgs = "";
   cargoVendorDir = vendorCargoDeps { cargoLock = ./tuic.lock; };
 
   CARGO_BUILD_TARGET = rustTarget;
