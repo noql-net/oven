56f6cee4fb410fdeedc17a98e95eda569332d266
Mark Pashmfouroush <mark@markpash.me>
2023-11-28 07:01:37 +0000

    sing-box: 1.6.5 -> 1.6.7
    Diff: https://github.com/SagerNet/sing-box/compare/v1.6.5...v1.6.7

--- go/sing-box.nix
+++ go/sing-box.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "sing-box";
-  version = "1.6.5";
+  version = "1.6.7";
 
   src = fetchFromGitHub {
     owner = "SagerNet";
     repo = "sing-box";
     rev = "v${version}";
-    hash = "sha256-djbRt4VdrZ2a0yLbNaFNhKIN0AwuCCJATIcwFhnw5aM=";
+    hash = "sha256-aER74HRU3yohIIhNel6KdmfDkrrSOiB8pkTf/hEA5uU=";
   };
 
   CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-qoW9+t427k5Ea9BhAdWIh+utD7EnIU1OLKJfsmYlEt8=";
+  vendorHash = "sha256-+L816RufHZ7TSoJF7HzCvS+GfWxeGBRXh7nOF0gYIZ8=";
 
   tags = [
     "with_gvisor"
