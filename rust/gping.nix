a12818c4d28add032cb6d4cc3240c2c3fcdd1d9e
Mark Pashmfouroush <mark@markpash.me>
2023-12-29 23:19:03 +0000

    gping: 1.15.1 -> 1.16.0

--- rust/gping.nix
+++ rust/gping.nix
@@ -2,13 +2,13 @@
 
 buildRustPackage rec {
   pname = "gping";
-  version = "1.15.1";
+  version = "1.16.0";
 
   src = fetchFromGitHub {
     owner = "orf";
     repo = "gping";
     rev = "gping-v${version}";
-    hash = "sha256-22Nio6yfkL9HWNrI+kk5dGfojTtB/h0sizCWH9w9so8=";
+    hash = "sha256-t9USry3I6tc8EKsfkq28/hPJMbaf0BqqOdzCl3oXd60=";
   };
 
   CARGO_BUILD_TARGET = rustTarget;
