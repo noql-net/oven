b23d12c70499fc5cae853fe5e8b9ca2f222a56fb
Mark Pashmfouroush <mark@markpash.me>
2023-12-29 23:54:46 +0000

    ooniprobe-cli: 3.20.0 -> 3.20.1
    Diff: https://github.com/ooni/probe-cli/compare/v3.20.0...v3.20.1

--- go/ooniprobe-cli.nix
+++ go/ooniprobe-cli.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "ooniprobe-cli";
-  version = "3.20.0";
+  version = "3.20.1";
 
   src = fetchFromGitHub {
     owner = "ooni";
     repo = "probe-cli";
     rev = "v${version}";
-    hash = "sha256-kOARV3tnRyOAScc3Vn96TFQFgqvTgi6NFHWM7ZbpciU=";
+    hash = "sha256-XjIrae4HPFB1Rv8yIAUh6Xj9UVU55Wx7SuyKJ0BvmXY=";
   };
 
   CGO_ENABLED = 1;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-c922VpxtpNfua3Sx3vXKz4x1FsLMwbaSvRH4dyFrr9s=";
+  vendorHash = "sha256-HYU+oS+iqdl2jQJc3h9T+MSc/Hq2W6UqP+oPSEyfcOU=";
 
   ldflags = [
     "-s"
