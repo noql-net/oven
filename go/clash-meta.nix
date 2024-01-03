0741f57648508324f9d118444a12238ad5a35715
Mark Pashmfouroush <mark@markpash.me>
2024-01-03 00:47:32 +0000

    clash-meta: 1.16.0 -> 1.18.0
    Diff: https://github.com/MetaCubeX/Clash.Meta/compare/v1.16.0...v1.18.0

--- go/clash-meta.nix
+++ go/clash-meta.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "clash-meta";
-  version = "1.16.0";
+  version = "1.18.0";
 
   src = fetchFromGitHub {
     owner = "MetaCubeX";
     repo = "Clash.Meta";
     rev = "v${version}";
-    hash = "sha256-ORyjCYf2OPrSt/juiBk0Gf2Az4XoZipKBWWFXf8nIqE=";
+    hash = "sha256-lxiPrFPOPNppxdm2Ns4jaMHMRCYFlMz2h2rf7x0gv9c=";
   };
 
   CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-ySCmHLuMTCxBcAYo7YD8zOpUAa90PQmeLLt+uOn40Pk=";
+  vendorHash = "sha256-b7q0e3HHolVhwNJF0kwvwuVy8ndJLc0ITMl+0/YtSjA=";
 
   tags = [
     "with_gvisor"
