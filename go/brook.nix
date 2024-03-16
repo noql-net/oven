4062dde2f37a8b360094c35289946f35f42e3b18
Mark Pashmfouroush <mark@markpash.me>
2024-03-16 16:32:49 +0000

    brook: 20240214 -> 20240404
    Diff: https://github.com/txthinking/brook/compare/v20240214...v20240404

--- go/brook.nix
+++ go/brook.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "brook";
-  version = "20240214";
+  version = "20240404";
 
   src = fetchFromGitHub {
     owner = "txthinking";
     repo = "brook";
     rev = "v${version}";
-    hash = "sha256-5+AqlmDa11PrB24XkelOFHK4sBi4j78WMLQrzDuP1/M=";
+    hash = "sha256-QqA0LnbC72bnAQ25AehTnoXgdqQPc8wztHcFd4Q19ko=";
   };
 
   CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-cTw9k4AqS4NOJ0vX0InR0xxOfCXIgA3FxgL6oXryOnA=";
+  vendorHash = "sha256-1aaOPeKHPrZO6WK08EhX4+dME0A33raQnbZi/aNFpIw=";
 
   ldflags = [
     "-s"
