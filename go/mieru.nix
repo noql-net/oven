0e6abeab9608d9b68618582de3b4fb5c2dc96953
Mark Pashmfouroush <mark@markpash.me>
2024-03-16 13:50:53 +0000

    mieru: 2.5.0 -> 2.6.0
    Diff: https://github.com/enfein/mieru/compare/v2.5.0...v2.6.0

--- go/mieru.nix
+++ go/mieru.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "mieru";
-  version = "2.5.0";
+  version = "2.6.0";
 
   src = fetchFromGitHub {
     owner = "enfein";
     repo = "mieru";
     rev = "v${version}";
-    hash = "sha256-gFTdflGCO0c8izidHW/+EFO8SWj035TUaMUhcqpjbdU=";
+    hash = "sha256-G8sZqYuUV9+7iOdwYngidcECF7haAYWVaPolyDFlBKg=";
   };
 
   CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-Y2ASenlPSPJRWkeLEuJnZ/g+HuBC0xJ/WntQyywI1p4=";
+  vendorHash = "sha256-kdaxYmFjfNoOuPcSNsAlOb8Byiqu7ZwEZ8mPTmwqQr8=";
 
   ldflags = [
     "-s"
