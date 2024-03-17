7dfe634e1610d204a5542a6057e5fea5e7a5e40e
Mark Pashmfouroush <mark@markpash.me>
2024-03-17 09:58:35 +0000

    shadowsocks-v2ray-plugin: 5.15.0 -> 5.15.1
    Diff: https://github.com/teddysun/v2ray-plugin/compare/v5.15.0...v5.15.1

--- go/shadowsocks-v2ray-plugin.nix
+++ go/shadowsocks-v2ray-plugin.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "shadowsocks-v2ray-plugin";
-  version = "5.15.0";
+  version = "5.15.1";
 
   src = fetchFromGitHub {
     owner = "teddysun";
     repo = "v2ray-plugin";
     rev = "v${version}";
-    hash = "sha256-0KzXMMvfhhSWyCZtPeNVb2eCurqMmBpArqhImXskjdU=";
+    hash = "sha256-Rer6Zz7gzAwunmFEVxEhsi8BzWGJiwRtxVZB58pGf7I=";
   };
 
   CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-JTp+TngPeZ8x9/JIT+UW2tErgCUgJ66KhTlaJlAZ7wc=";
+  vendorHash = "sha256-XEdZiA5DpM+VuoLX9G12dh2/FI2f2TY5KxSVlKHzJDY=";
 
   ldflags = [
     "-s"
