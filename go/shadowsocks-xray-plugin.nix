964bd882ac42e5bc11a3bce60462aea28af267b3
Mark Pashmfouroush <mark@markpash.me>
2024-08-09 05:08:02 +0000

    shadowsocks-xray-plugin: 1.8.12 -> 1.8.15
    Diff: https://github.com/teddysun/xray-plugin/compare/v1.8.12...1.8.15

--- go/shadowsocks-xray-plugin.nix
+++ go/shadowsocks-xray-plugin.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "shadowsocks-xray-plugin";
-  version = "1.8.12";
+  version = "1.8.15";
 
   src = fetchFromGitHub {
     owner = "teddysun";
     repo = "xray-plugin";
     rev = "v${version}";
-    hash = "sha256-U/2LsOz5yVcQ93gY1rjV955YJF8ci6nE/1Em5Cg+xBA=";
+    hash = "sha256-vczlNr0z0UM36ZZyaSGGSFyx+eqYRKCgYHt9kXFsYok=";
   };
 
   CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-sYkoLmLWOV/QKkAn+TMFWRlLyvmJ3El592Bqchi8bD0=";
+  vendorHash = "sha256-YCrbXsC7MCFJJCkR9FyOahcdoCReGF+wXX0NVBej5oM=";
 
   ldflags = [
     "-s"
