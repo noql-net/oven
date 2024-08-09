0d2433db5cbdbd3f916d954cf8985dcdc18fd579
Mark Pashmfouroush <mark@markpash.me>
2024-08-09 04:42:44 +0000

    xray-knife: 2.11.16 -> 2.12.17
    Diff: https://github.com/lilendian0x00/xray-knife/compare/v2.11.16...2.12.17

--- go/xray-knife.nix
+++ go/xray-knife.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "xray-knife";
-  version = "2.11.16";
+  version = "2.12.17";
 
   src = fetchFromGitHub {
     owner = "lilendian0x00";
     repo = "xray-knife";
     rev = "v${version}";
-    hash = "sha256-MdiqX21X7VMyh8L+SBKcXTCUVE9d1EpXcPy5IqQL4oc=";
+    hash = "sha256-O5mB49fEulurduauWise4cvH1nWATX4JHSD6WLZfsME=";
   };
 
   CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-sp+IUP5c1+vZyD4jh+uj8vW/MCv/LxEeePwr7VLYtb4=";
+  vendorHash = "sha256-AzHPtZIydi+O+r8sK0SN1WNFp6cOxuLrDVb8+DL4JM0=";
 
   ldflags = [
     "-s"
