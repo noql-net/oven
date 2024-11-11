1695e6f1edaaa6340b5e8b3d44af87d74f2935dd
Mark Pashmfouroush <mark@markpash.me>
2024-11-11 05:56:33 +0000

    shadowsocks-rust: 1.19.3 -> 1.19.4
    Diff: https://github.com/shadowsocks/shadowsocks-rust/compare/v1.19.3...1.19.4
    
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- rust/shadowsocks-rust.nix
+++ rust/shadowsocks-rust.nix
@@ -2,13 +2,13 @@
 
 buildRustPackage rec {
   pname = "shadowsocks-rust";
-  version = "1.19.3";
+  version = "1.19.4";
 
   src = fetchFromGitHub {
     owner = "shadowsocks";
     repo = "shadowsocks-rust";
     rev = "v${version}";
-    hash = "sha256-NWUIIEK0SYLNr2vTKUrcPvzCA9dGPFvDkAxKHHg5DQo=";
+    hash = "sha256-QaKLx8eIFdLngpdktR9ReYLKzXkZfQIlhFfM8K84jdc=";
   };
 
   buildFeatures = [
