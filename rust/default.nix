c1c85df6d1ebbda831bfbfe3e17cbaf4faffb264
Mark Pashmfouroush <mark@markpash.me>
2025-04-12 13:48:50 +0100

    remove shadow-tls
    Signed-off-by: Mark Pashmfouroush <mark@markpash.me>

--- rust/default.nix
+++ rust/default.nix
@@ -14,16 +14,6 @@ let
     };
   };
 
-  nightlyToolchain = oxalica-rust.packages."${buildSystem}".rust-nightly_2023-12-17.override {
-    targets = [ "x86_64-unknown-linux-musl" "aarch64-unknown-linux-musl" ];
-  };
-  nightlyCraneLib = (crane.mkLib pkgs).overrideToolchain nightlyToolchain;
-  buildPackageNightly = nightlyCraneLib.buildPackage.override {
-    mkCargoDerivation = nightlyCraneLib.mkCargoDerivation.override {
-      stdenv = targetPkgs.pkgsStatic.buildPackages.stdenv;
-    };
-  };
-
   rustTarget = pkgs.rust.toRustTarget targetPkgs.pkgsStatic.hostPlatform;
   targetCC = with targetPkgs.pkgsStatic.stdenv; "${cc}/bin/${cc.targetPrefix}cc";
 
@@ -31,9 +21,6 @@ let
   openssl = targetPkgs.pkgsStatic.openssl;
 in
 {
-  shadow-tls = (import ./shadow-tls.nix) {
-    inherit lib fetchFromGitHub rustTarget targetCC;  buildRustPackage = buildPackageNightly;
-  };
   shadowsocks-rust = (import ./shadowsocks-rust.nix) {
     inherit lib fetchFromGitHub rustTarget targetCC pkg-config openssl; buildRustPackage = buildPackageStable;
   };
