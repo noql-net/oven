5bf562a0670836e7fe13519dddf0610f25d95e39
Mark Pashmfouroush <mark@markpash.me>
2025-04-21 10:54:41 +0000

    gost: 3.0.0-rc10 -> 3.0.0
    Diff: https://github.com/go-gost/gost/compare/v3.0.0-rc10...v3.0.0

--- go/gost.nix
+++ go/gost.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "gost";
-  version = "3.0.0-rc10";
+  version = "3.0.0";
 
   src = fetchFromGitHub {
     owner = "go-gost";
     repo = "gost";
     rev = "v${version}";
-    hash = "sha256-o0lE6grS7beslw/O0AtmPxeWlA4z8XAaLlo41U6zAsg=";
+    hash = "sha256-ep3ZjD+eVKl3PuooDuYeur8xDAcyy6ww2I7f3cYG03o=";
   };
 
   env.CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-C4HPOPveU+pOd8gyZWSo9R5EAHh+RvrfqtlpiJoeW84=";
+  vendorHash = "sha256-lzyr6Q8yXsuer6dRUlwHEeBewjwGxDslueuvIiZUW70=";
 
   ldflags = [
     "-s"
