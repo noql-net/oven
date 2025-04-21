ab5a06720e961cb11999f2807981f33165676286
Mark Pashmfouroush <mark@markpash.me>
2025-04-21 12:58:18 +0000

    headscale: 0.24.3 -> 0.25.0
    Diff: https://github.com/juanfont/headscale/compare/v0.24.3...v0.25.0

--- go/headscale.nix
+++ go/headscale.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "headscale";
-  version = "0.24.3";
+  version = "0.25.0";
 
   src = fetchFromGitHub {
     owner = "juanfont";
     repo = "headscale";
     rev = "v${version}";
-    hash = "sha256-G1MGu/AVFhiBOfhmhdH9+kayPybtsQsvj73omtZVfBU=";
+    hash = "sha256-5CwaPaGh0yvHwmSpbsvc4ajkW9RbYVMilNTIJxeYcIs=";
   };
 
   env.CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-SBfeixT8DQOrK2SWmHHSOBtzRdSZs+pwomHpw6Jd+qc=";
+  vendorHash = "sha256-ZQj2A0GdLhHc7JLW7qgpGBveXXNWg9ueSG47OZQQXEw=";
 
   tags = [ "ts2019" ];
   ldflags = [
