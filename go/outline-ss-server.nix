e0c0b3b887f8b9d5a582c627718d0a3d6e2322af
Mark Pashmfouroush <mark@markpash.me>
2025-04-21 12:10:07 +0000

    outline-ss-server: 1.7.3 -> 1.8.0
    Diff: https://github.com/Jigsaw-Code/outline-ss-server/compare/v1.7.3...v1.8.0

--- go/outline-ss-server.nix
+++ go/outline-ss-server.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "outline-ss-server";
-  version = "1.7.3";
+  version = "1.8.0";
 
   src = fetchFromGitHub {
     owner = "Jigsaw-Code";
     repo = "outline-ss-server";
     rev = "v${version}";
-    hash = "sha256-lY1CB5szAHgm4/RO1xHG2rCisXq379BfHrKG/2UQUGo=";
+    hash = "sha256-o6ZVIfSvhF7PkMov1WtHrn3hWk/rqT3tDjge/hfJE9k=";
   };
 
   env.CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-qcKOF+19R5QQkM10AcGeetPRA3gJAe5Rn6CR+TU5zZA=";
+  vendorHash = "sha256-/hPh/4805S9B8PlTDcNNRegTr4NZ+QaYq6BqI0SYlnQ=";
 
   ldflags = [
     "-s"
