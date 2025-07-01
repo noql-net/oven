7eed9d1e78ffa52ca7eb698e1c2fd85804ec0e48
Mark Pashmfouroush <mark@markpash.me>
2025-07-01 14:58:29 +0000

    psiphon-tunnel-core: 2.0.31 -> 2.0.32
    Diff: https://github.com/Psiphon-Labs/psiphon-tunnel-core/compare/v2.0.31...v2.0.32

--- go/psiphon-tunnel-core.nix
+++ go/psiphon-tunnel-core.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "psiphon-tunnel-core";
-  version = "2.0.31";
+  version = "2.0.32";
 
   src = fetchFromGitHub {
     owner = "Psiphon-Labs";
     repo = "psiphon-tunnel-core";
     rev = "v${version}";
-    hash = "sha256-269gn5AVjBtNsRBxtQYhT8oK0Cg4TjU6HKtbyPsZu7s=";
+    hash = "sha256-fntHN9YsAWUD85YebEp7Kl0dnh6wG4fr74GBf5olSB4=";
   };
 
   env.CGO_ENABLED = 0;
