b93d6943d5f697384f654b69303a9613b53f0b8f
Mark Pashmfouroush <mark@markpash.me>
2025-07-01 18:33:55 +0000

    daze: 1.22.0 -> 1.23.0
    Diff: https://github.com/mohanson/daze/compare/v1.22.0...v1.23.0

--- go/daze.nix
+++ go/daze.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "daze";
-  version = "1.22.0";
+  version = "1.23.0";
 
   src = fetchFromGitHub {
     owner = "mohanson";
     repo = "daze";
     rev = "v${version}";
-    hash = "sha256-jjfOzQrIxS699Yg/rG21eu8O5QsI28jHqwjghHenWKw=";
+    hash = "sha256-aIVFi5UdsxB8ghU43IAR4UdXQFQXBRgA3WroyacXlyM=";
   };
 
   env.CGO_ENABLED = 0;
