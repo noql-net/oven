b6d300365e5d05dc94780badab3dc12f6e8c7be3
Mark Pashmfouroush <mark@markpash.me>
2025-07-01 17:40:40 +0000

    mihomo: 1.19.9 -> 1.19.10
    Diff: https://github.com/MetaCubeX/mihomo/compare/v1.19.9...v1.19.10

--- go/mihomo.nix
+++ go/mihomo.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "mihomo";
-  version = "1.19.9";
+  version = "1.19.10";
 
   src = fetchFromGitHub {
     owner = "MetaCubeX";
     repo = "mihomo";
     rev = "v${version}";
-    hash = "sha256-T1tg4sAR7OY3N6OheUySyQ0HLXYUBTLjeWx3qxHsb30=";
+    hash = "sha256-7zbJMwczbCHRrAbRC61Pjo58dyphTu+3WYkfi3iiwxw=";
   };
 
   env.CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-+7D2XAmN2AF2N1tmE1O7378duQYX5fgkUDxeO4n7Glk=";
+  vendorHash = "sha256-2hLI8R6hwam7/n8JRQ9Xs4ycWwdNxGRvFM+JhH7ExgM=";
 
   tags = [
     "with_gvisor"
