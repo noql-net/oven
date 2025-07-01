3d02bc3ed034299b4719dd0bf46ff9224c088cbb
Mark Pashmfouroush <mark@markpash.me>
2025-07-01 23:20:10 +0000

    dnscrypt-proxy2: 2.1.9 -> 2.1.10
    Diff: https://github.com/DNSCrypt/dnscrypt-proxy/compare/2.1.9...2.1.10

--- go/dnscrypt-proxy2.nix
+++ go/dnscrypt-proxy2.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "dnscrypt-proxy2";
-  version = "2.1.9";
+  version = "2.1.10";
 
   src = fetchFromGitHub {
     owner = "DNSCrypt";
     repo = "dnscrypt-proxy";
     rev = "${version}";
-    hash = "sha256-8KnanJw9eBFm/zdy6f4OFCMStzic/n4Alnm5Y/pbDCA=";
+    hash = "sha256-hAw7F1TMjxEJt5U1SWF+A/Bh2yt/8DOdn0infPZF6kk=";
   };
 
   env.CGO_ENABLED = 0;
