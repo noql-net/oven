2df1dcb70d3a516f78c9cefd60dc1f5f61a61b8b
Mark Pashmfouroush <mark@markpash.me>
2025-10-28 11:22:50 +0000

    gost: 3.2.3 -> 3.2.4
    Diff: https://github.com/go-gost/gost/compare/v3.2.3...v3.2.4

--- go/gost.nix
+++ go/gost.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "gost";
-  version = "3.2.3";
+  version = "3.2.4";
 
   src = fetchFromGitHub {
     owner = "go-gost";
     repo = "gost";
     rev = "v${version}";
-    hash = "sha256-B2ffIGS0tURZI7uVALih7YM9thBWAjihdIdSHhQFnE8=";
+    hash = "sha256-zFGoM+fYO/o70LJ2fbzhMv4qbjLozJGmGbudqHjCFRU=";
   };
 
   env.CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-WvCNvmH/KpFivKMBqBMgKi9VieOegGIrh463YC9WwAg=";
+  vendorHash = "sha256-W3CHttD9iGpl2wG/Pa21YY6ACf5M894p25nLfMX0+F4=";
 
   ldflags = [
     "-s"
