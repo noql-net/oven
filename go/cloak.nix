e8366946fc28a5627cd392408ee772fa9530a09b
Mark Pashmfouroush <mark@markpash.me>
2025-10-28 11:49:11 +0000

    cloak: 2.11.0 -> 2.12.0
    Diff: https://github.com/cbeuw/Cloak/compare/v2.11.0...v2.12.0

--- go/cloak.nix
+++ go/cloak.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "cloak";
-  version = "2.11.0";
+  version = "2.12.0";
 
   src = fetchFromGitHub {
     owner = "cbeuw";
     repo = "Cloak";
     rev = "v${version}";
-    hash = "sha256-afFOWjJiqlMeo8M8D2RsW572c2qTthMNbQvxEf7edHE=";
+    hash = "sha256-789UyTJmIhujsg0OlCy8GqUxgHDjzkGUi5kHD5sytwQ=";
   };
 
   env.CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-P3/fB1vJjEMETyFxH9XNQySCEDQWrbZdaf0V4qFucbI=";
+  vendorHash = "sha256-LOXPs/3qkP3GJZZ7W4rPOfAjmvNh1mowRuQ1tlV1uC4=";
 
   ldflags = [
     "-s"
