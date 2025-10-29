6ce89d3b63824823dd35310a3c3936e9243c901a
Mark Pashmfouroush <mark@markpash.me>
2025-10-29 12:09:57 +0000

    mieru: 3.19.1 -> 3.19.2
    Diff: https://github.com/enfein/mieru/compare/v3.19.1...v3.19.2

--- go/mieru.nix
+++ go/mieru.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "mieru";
-  version = "3.19.1";
+  version = "3.19.2";
 
   src = fetchFromGitHub {
     owner = "enfein";
     repo = "mieru";
     rev = "v${version}";
-    hash = "sha256-x8rddxjhmHw7J7majt4qdkXRPsfm8SATFsMxN2stN14=";
+    hash = "sha256-s5e1P9dixcvbCFqNF/dztX93sh63Wiwa052VQ3XVHI4=";
   };
 
   env.CGO_ENABLED = 0;
