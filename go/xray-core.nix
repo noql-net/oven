edb42b9a25e54b3b2dafce842c598d570f74d303
Mark Pashmfouroush <mark@markpash.me>
2025-10-29 20:54:20 +0000

    xray-core: 25.6.8 -> 25.7.26
    Diff: https://github.com/XTLS/Xray-core/compare/v25.6.8...v25.7.26

--- go/xray-core.nix
+++ go/xray-core.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "xray-core";
-  version = "25.6.8";
+  version = "25.7.26";
 
   src = fetchFromGitHub {
     owner = "XTLS";
     repo = "Xray-core";
     rev = "v${version}";
-    hash = "sha256-XVeocGTOd2rL6d8e2hvcwZDl+GMNMjXxCDJ8JRYuY54=";
+    hash = "sha256-oPB4OngMDUk+HURDoUhIZ6SGQ0gII740PjgjP2fUK/k=";
   };
 
   env.CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-waddDTa6X9q2gcpSdJgmBg+eaKKZiCJcmsRCnGcT83Y=";
+  vendorHash = "sha256-AmY9q0I0S7Xki9mgCJn/nV4Y9Ra6MDgnma2iPnoWQhg=";
 
   ldflags = [
     "-s"
