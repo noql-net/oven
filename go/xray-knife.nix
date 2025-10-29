4dda2efd5a0478b5b68a91fd5482a1c372180500
Mark Pashmfouroush <mark@markpash.me>
2025-10-29 15:04:46 +0000

    xray-knife: 6.2.6 -> 7.0.0
    Diff: https://github.com/lilendian0x00/xray-knife/compare/v6.2.6...v7.0.0

--- go/xray-knife.nix
+++ go/xray-knife.nix
@@ -2,19 +2,19 @@
 
 buildGoModule rec {
   pname = "xray-knife";
-  version = "6.2.6";
+  version = "7.0.0";
 
   src = fetchFromGitHub {
     owner = "lilendian0x00";
     repo = "xray-knife";
     rev = "v${version}";
-    hash = "sha256-JfvrE6XmZ31srL5onlAAeFjyHH2f4bPwmYWkBPOS+wA=";
+    hash = "sha256-eCabQgu8MYqLhI3zlwZsnUKCsn34zecB1A+EHl8oarg=";
   };
 
   env.CGO_ENABLED = 0;
   hardeningDisable = [ "pie" ];
 
-  vendorHash = "sha256-PcZj3jaU7Pi5CgpTS8eB1KAh/dnUGXo2dve9dNqHIEU=";
+  vendorHash = "sha256-kbbthWXtlkoQtCvf8vQNfiZgEgAM0fDRtTbFRQbnl6A=";
 
   ldflags = [
     "-s"
